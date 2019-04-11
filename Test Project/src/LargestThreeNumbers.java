a823e393c291ecd529f089d571ed9332ab21d036
Aytha150427 <Aytha150427@MY-KLM1LW100019.majesco.com>
2019-04-11 10:07:31 +0800

    Written Test Programms

---
+++ Test Project/src/LargestThreeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LargestThreeNumbers {
+
+	public static void main(String[] args) {
+
+		List<Integer> largest = new ArrayList<Integer>();
+		largest.add(1);
+		largest.add(2);
+		largest.add(3);
+		largest.add(4);
+		largest.add(5);
+		largest.add(6);
+
+		Collections.sort(largest, Collections.reverseOrder());
+		
+		for (Integer desc : largest) {
+			if(desc > 3){
+				System.out.println(desc);
+			}
+		}
+	}
+}
---
+++ Test Project/src/SecondHighestNumberInArray.java
@@ -0,0 +1,36 @@
+public class SecondHighestNumberInArray {
+
+    public static void main(String[] args) {
+
+             int arr[] = { 14, 46, 47, 45, 92, 52, 48, 36, 66, 85 };
+             
+             System.out.println(arr.length);
+             
+             int largest = arr[0], secondLargest = arr[0];
+             
+             System.out.println(largest);
+             
+             System.out.println(secondLargest);
+
+             for (int i = 0; i < arr.length; i++) {   
+            	 
+            	 System.out.println(arr[i]);
+            	 
+                 if (arr[i] > largest) {
+                	 System.out.println("INSIDE IF");
+                        secondLargest = largest;
+                        largest = arr[i];
+
+               } else if (arr[i] > secondLargest) {
+            	   System.out.println("INSIDE ELSE IF");
+                        secondLargest = arr[i];
+               }
+             }
+             
+
+             System.out.println("\nSecond largest number is:" + secondLargest);
+
+    }
+}
+
+
