a366d46ac08ef278783ec664857a26b6377de685
Aytha150427 <Aytha150427@MY-KLM1LW100019.majesco.com>
2019-04-11 16:54:10 +0800

    Added Prpograms

---
+++ Test Project/src/Allianze.java
@@ -0,0 +1,11 @@
+public class Allianze {
+	public static void main(String[] args) {
+		int x = 5, y = ++x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
+
+
+
+
---
+++ Test Project/src/MathProgramme.java
@@ -0,0 +1,25 @@
+/*A test had 20 questions. 
+ * Each correct answer was worth 10 points, but 5 point was taken off for each incorrect answer or missing answer. 
+ * The test score was 125. How many questions are correct and how many or wrong*/
+
+public class MathProgramme {
+
+	public static void main(String[] args) {
+		/*a+b=20;
+		 *10a-5b = 125;
+		 *
+		 *b = 20-a;
+		 *
+		 *10a-5(20-a) = 125
+		 *10a -100+5a = 125
+		 *15a = 125+100
+		 *15a = 225
+		 *a = 225/15
+		 *a = 15
+		 *15*9 = 135-125 = 10
+		 *
+		 *a= 15 and b = 10
+		 * */
+	}
+
+}
